c488301b5d9bd0717bc0181fe31f79e6af66e5cc
Jacob Wirth <xthexder@live.com>
2014-06-04 03:42:36 -0400

    Bit of clean up, change to range shuffle for mines

--- game.js
+++ game.js
@@ -4,25 +4,52 @@
  * Author: Jacob Wirth
  */
 
-/* Game states:
-  0 - New game
-  1 - Game running
-  2 - Game over
+/*
+  Board size: 30 x 16 = 480
+
+  Game states:
+    0 - New game
+    1 - Game running
+    2 - Game over
 
   Game modes:
-  0 - Normal
+    0 - Normal
 */
 
-var gvars = {state: 0, mode: 0, game: null, tiles: null, board: [], mines: []};
+var gvars = {
+  state: 0,
+  mode: 0,
 
-function shuffle(mines) {
-    for (var i = mines.length - 1; i >= 0; i--) {
-      var j = Math.floor(Math.random() * i);
-      var tmp = mines[i];
-      mines[i] = mines[j];
-      mines[j] = tmp;
-    }
+  game: null,
+  tiles: null,
+
+  board: [],
+  mines: [],
+
+  knownMines: 0,
+  knownSafe: 0
 };
+
+function shuffleRange(mines, start, stop) {
+  var len = stop - start;
+  for (var i = start; i < stop; i++) {
+    var j = Math.floor(Math.random() * len);
+    var tmp = mines[i];
+    mines[i] = mines[start + j];
+    mines[start + j] = tmp;
+  }
+}
+
+// callback(tilei)
+function eachNeighbour(tilei, callback) {
+  var a = (tilei % 30 > 0) ? -1 : 0;
+  var b = (tilei % 30 < 29) ? 1 : 0;
+  for (var i = a; i <= b; i++) {
+    if (tilei >= 30) callback(tilei + i - 30);
+    callback(tilei + i);
+    if (tilei < 450) callback(tilei + i + 30);
+  }
+}
 
 function renderEnter(tiles) {
   var tile = tiles.append("div")
@@ -39,34 +66,39 @@ function renderUpdate(tile) {
     .html(function(d) { return d > 0 ? d : ""; });
 }
 
+function initBoard() {
+  for (var i = 0; i < 480; i++) {
+    gvars.board[i] = 0;
+    gvars.mines[i] = i;
+  };
+}
+
 function clickMine(tile, tilei) {
   if (gvars.state === 0) {
-    for (var i = 0, j = 0; i < 30 * 16 - 9; i++, j++) {
-      if (j === tilei - 31 || j === tilei - 1 || j === tilei + 29) j += 3;
-      gvars.mines[i] = j;
-    };
-    shuffle(gvars.mines);
-    for (var i = 0; i < 99; i++) {
-      var mine = gvars.mines[i];
-      gvars.board[mine] = -1;
-      var a = (mine % 30 > 0) ? -1 : 0;
-      var b = (mine % 30 < 29) ? 1 : 0;
-      for (var j = a; j <= b; j++) {
-        if (mine >= 30 && gvars.board[mine + j - 30] >= 0) gvars.board[mine + j - 30]++;
-        if (gvars.board[mine + j] >= 0) gvars.board[mine + j]++;
-        if (mine < 30 * 15 && gvars.board[mine + j + 30] >= 0) gvars.board[mine + j + 30]++;
-      }
-    }
+    // No mines within 1 tile of the first click
+    gvars.knownSafe = 0;
+    eachNeighbour(tilei, function(tilei) {
+      gvars.knownSafe++;
+      gvars.mines[480 - gvars.knownSafe] = tilei;
+      gvars.mines[tilei] = 480 - gvars.knownSafe;
+    });
     gvars.state = 1;
+  } else return
+
+  shuffleRange(gvars.mines, gvars.knownMines, 480 - gvars.knownSafe);
+  for (var i = 0; i < 99; i++) {
+    gvars.board[gvars.mines[i]] = -1;
+    eachNeighbour(gvars.mines[i], function(mine) {
+      if (gvars.board[mine] >= 0) gvars.board[mine]++;
+    });
   }
+
   renderUpdate(gvars.tiles);
 }
 
 document.addEventListener('DOMContentLoaded', function(){
   gvars.game = d3.select("#game");
-  for (var i = 0; i < 30 * 16; i++) {
-    gvars.board[i] = 0;
-  };
+  initBoard();
 
   gvars.tiles = gvars.game.selectAll(".tile").data(gvars.board);
   renderEnter(gvars.tiles.enter());
